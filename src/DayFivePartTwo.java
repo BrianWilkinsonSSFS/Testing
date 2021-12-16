354e51d00934b3bd8d0aa26df384e64bc8f06f3b
wilkinsonbrian <wilkibrian@gmail.com>
2021-12-16 08:41:27 -0500

    Finished Day 5 Part 1 - Answer for part 2 too low

--- src/DayFivePartTwo.java
+++ src/DayFivePartTwo.java
@@ -1,12 +1,31 @@
-import java.awt.Point;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-public class DayFivePartOne {
+public class DayFivePartTwo {
     private static Set<Point> duplicates = new HashSet<>();
+
+    public static void addDiagonalPoints(Point p1, Point p2, Set<Point> points) {
+        int distance = Math.abs(p1.x - p2.x);
+        if (p1.y < p2.y) {
+            for (int i=1; i<=distance; i++) {
+                Point newP = new Point(p1.x+i, p1.y+i);
+                if (!points.add(newP)) {
+                    duplicates.add(newP);
+                }
+            }
+        } else {
+            for (int j=1; j<=distance; j++) {
+                Point newP = new Point(p1.x+j, p1.y-j);
+                if (!points.add(newP)) {
+                    duplicates.add(newP);
+                }
+            }
+        }
+    }
 
     public static void addPointsToSet(Point p1, Point p2, Set<Point> points) {
 
@@ -67,6 +86,8 @@ public class DayFivePartOne {
             Point[] p = getPoints(scan.nextLine());
             if (isVorH(p[0], p[1]))
                 addPointsToSet(p[0], p[1], points);
+            else
+                addDiagonalPoints(p[0], p[1], points);
         }
         scan.close();
         System.out.println(duplicates.size());
